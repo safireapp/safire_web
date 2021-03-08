643d21142cae0dabd53c36b02f536bf2cf9c17aa
faisalsayed10 <faisal.sayed502@gmail.com>
2021-03-08 15:36:43 +0530

    get user details and post user details  api route

--- pages/api/user.ts
+++ pages/api/user.ts
@@ -1,6 +1,32 @@
+import { PrismaClient } from "@prisma/client";
+import { NextApiRequest, NextApiResponse } from "next";
+import { Session } from "next-iron-session";
 import withSession from "../../lib/session";
+import { User } from "../../util/types";
+import { reduceUserDetails } from "../../util/userValidators";
 
-export default withSession(async (req, res) => {
-  const user = req.session.get("user");
-  res.json(user);
-});
+const prisma = new PrismaClient();
+
+export default withSession(
+  async (req: NextApiRequest & { session: Session }, res: NextApiResponse) => {
+    const user: User = await req.session.get("user");
+    if (!user) return res.json({ message: "You are not logged in" });
+
+    if (req.method === "GET") {
+      // Simply get the user
+      return res.json(user);
+    } else if (req.method === "POST") {
+      // Update the user details
+      const userDetails = reduceUserDetails(req.body);
+      const updatedUser = await prisma.user.update({
+        where: { id: user.id },
+        data: userDetails,
+      });
+
+      req.session.set("user", updatedUser);
+      await req.session.save();
+
+      return res.json(updatedUser);
+    }
+  }
+);
