6ec1ea8c99953bd66a9474668c11ad5f6247515b
faisalsayed10 <faisal.sayed502@gmail.com>
2021-03-11 11:38:14 +0530

    Like Dislike API Route

---
+++ pages/api/post/[postId]/like.ts
@@ -0,0 +1,47 @@
+// Like / Dislike API route
+
+import withSession from "@lib/session";
+import { PrismaClient, User } from "@prisma/client";
+import { NextApiRequest, NextApiResponse } from "next";
+import { Session } from "next-iron-session";
+
+const prisma = new PrismaClient();
+
+export default withSession(
+  async (req: NextApiRequest & { session: Session }, res: NextApiResponse) => {
+    try {
+      const postId = req.query.postId as string;
+      const user: User = await req.session.get("user");
+      if (!user)
+        return res.status(403).json({ message: "You are not logged in!" });
+
+      const like = await prisma.like.findFirst({
+        where: {
+          postId,
+          userId: user.id,
+        },
+      });
+
+      if (!like) {
+        await prisma.like.create({
+          data: {
+            postId,
+            userId: user.id,
+          },
+        });
+        return res.json({ message: "Liked the post" });
+      } else {
+        await prisma.like.deleteMany({
+          where: {
+            postId,
+            userId: user.id,
+          },
+        });
+        return res.json({ message: "Disliked the post" });
+      }
+    } catch (err) {
+      console.error(err);
+      return res.status(500).json({ message: err.message });
+    }
+  }
+);
