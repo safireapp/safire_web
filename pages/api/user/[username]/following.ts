a002eab68e55a8ef4150804c8c08002425162b5b
faisalsayed10 <faisal.sayed502@gmail.com>
2021-03-15 20:19:38 +0530

    get a list of user following

---
+++ pages/api/user/[username]/following.ts
@@ -0,0 +1,45 @@
+// Get a list of users who are followed by the user
+
+import withSession from "@lib/session";
+import { PrismaClient } from "@prisma/client";
+import { NextApiRequest, NextApiResponse } from "next";
+import { Session } from "next-iron-session";
+
+const prisma = new PrismaClient();
+
+export default withSession(
+  async (req: NextApiRequest & { session: Session }, res: NextApiResponse) => {
+    try {
+      const username = req.query.username as string;
+      const followingList = await prisma.user.findUnique({
+        where: {
+          username,
+        },
+        select: {
+          following: true,
+        },
+      });
+
+      const users = await prisma.user.findMany({
+        where: {
+          username: {
+            in: followingList.following,
+          },
+        },
+        select: {
+          id: true,
+          username: true,
+          firstName: true,
+          lastName: true,
+          imageUrl: true,
+          verified: true
+        }
+      });
+
+      return res.json(users);
+    } catch (err) {
+      console.error(err);
+      return res.status(500).json({ message: err.message });
+    }
+  }
+);
