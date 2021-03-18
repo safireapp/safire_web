4088110896aff6c23ed3f5885f31d8d4744e35ed
faisalsayed10 <faisal.sayed502@gmail.com>
2021-03-18 15:44:18 +0530

    updates in the context

--- context/useAuth.tsx
+++ context/useAuth.tsx
@@ -1,6 +1,6 @@
 import { User } from ".prisma/client";
-import { SignupData } from "@utils/types";
-import axios from "axios";
+import { LoginData, SignupData } from "@utils/types";
+import axios, { AxiosResponse } from "axios";
 import { createContext, useContext, useMemo, useState } from "react";
 
 const UserContext = createContext(null);
@@ -18,17 +18,17 @@ function useAuthProvider() {
   const [user, setUser] = useState<User>(null);
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
-  // const [errors, setErrors] = 
-  const [loading, setLoading] = useState<boolean>(false)
+  // const [errors, setErrors] =
+  const [loading, setLoading] = useState<boolean>(false);
 
   const signup = async (userDetails: SignupData) => {
-    const { data } = await axios.post("/api/signup");
+    const { data } = await axios.post("/api/signup", userDetails);
     setUser(data);
   };
 
   const contextValue = useMemo(() => {
-    return { user, signup, email, password, setEmail, setPassword, loading };
-  }, [user, signup, email, password, setEmail, setPassword, loading]);
+    return { user, signup, email, password, setEmail, setPassword, loading, setLoading, setUser };
+  }, [user, signup, email, password, setEmail, setPassword, loading, setLoading, setUser]);
 
   return contextValue;
 }
